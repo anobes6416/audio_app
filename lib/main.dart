f8ad8b409a94cd59db27dff267b65745dfb0a162
anobes6416 <sebonaifa777@gmail.com>
2023-09-21 15:15:11 -0400

    audioflutter commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,69 @@ void main(){
       MaterialApp(
       debugShowCheckedModeBanner: false,
       color: AppColors.background,
-      home:const Scaffold(
+      home: Scaffold(
           body: Column(
             children: [
-              Row(
+              Container(
+                margin: EdgeInsets.fromLTRB(20,40,20,0),
+                 child: const Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  ImageIcon(AssetImage("assets/menu.png"), size: 24, color: Colors.black),
+                  ImageIcon(AssetImage("assets/menu.png"), size: 45, color: Colors.black),
                   Row(
                     children: [
                       Icon(Icons.search),
                       Icon(Icons.notifications)
                     ],
-                  ),
+                  ),  
                 ],
+               ),
               ),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 20),
+                  child: Text(
+                    "Popular Books",
+                    style: TextStyle(fontSize: 30),
+                    ),
+                ),
+              ],
+            ),
+           Container(
+            height: 180,
+            child: Stack(
+              children: [
+                Positioned(
+                  left: -20,
+                  top: 0,
+                  right: 0,
+                  bottom: 0,
+
+                  child: Container(
+              height: 150,
+              child: PageView.builder(
+                controller: PageController(viewportFraction: 0.8), 
+                itemCount: 5,
+                itemBuilder: (_, i){
+                  return Container(
+                    height: 180,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        image: AssetImage("assets/sweetmess.jpeg"),
+                        fit: BoxFit.fill,
+                        ),
+                    ),
+                  );
+                },
+              ),
+            ), 
+                  ),
+              ],
+            ),
+           ),
+          
             ],
           ),
          ),
